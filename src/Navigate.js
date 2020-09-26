84d68a273a305998b0d8a24e4e3715e6dc47cb2c
Brayan Garcia <bgarcia.95@outlook.com>
2020-09-25 21:54:10 -0600

    Add Screens to stack navigator

--- src/Navigate.js
+++ src/Navigate.js
@@ -1,41 +1,52 @@
-import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
 
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import Home from './Home';
+import Home from "./Home";
+import PantallaLogin from "./screens/Auth/PantallaLogin";
+import Registrarse from "./screens/Auth/Registrarse";
+import RecuperarContrasena from "./screens/Auth/RecuperarContrasena";
 
 const Navegador = (props) => {
-	const Stack = createStackNavigator();
+  const Stack = createStackNavigator();
 
-	return (
-		<NavigationContainer>
-			<Stack.Navigator
-				screenOptions={{
-					headerShown: false,
-				}}
-				initialRouteName="Home"
-			>
-				<Stack.Screen name="Home" component={Home} />
-			</Stack.Navigator>
-		</NavigationContainer>
-	);
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+        initialRouteName="PantallaLogin"
+      >
+        {/* Login Process */}
+        <Stack.Screen name="PantallaLogin" component={PantallaLogin} />
+        <Stack.Screen name="Registrarse" component={Registrarse} />
+        <Stack.Screen
+          name="RecuperarContrasena"
+          component={RecuperarContrasena}
+        />
+
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 const styles = StyleSheet.create({
-	container: {
-		height: '100%',
-		width: '100%',
-		alignItems: 'center',
-		justifyContent: 'flex-start',
-		//backgroundColor: '#e1e8f4',
-	},
+  container: {
+    height: "100%",
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    //backgroundColor: '#e1e8f4',
+  },
 });
 
 const mapStateToProps = (state) => ({
-	login: state.login,
+  login: state.login,
 });
 
 export default connect(mapStateToProps)(Navegador);
