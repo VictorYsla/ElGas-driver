61fd2d3df0298300624c4627babbafb205761d8e
Brayan Garcia <bgarcia.95@outlook.com>
2020-09-28 18:12:22 -0600

    Add navigation file for redux

--- src/redux/reducers/navigation.js
+++ src/redux/reducers/navigation.js
@@ -1,20 +1,20 @@
-export const ACTUALIZAR_NAVIGATION = 'ACTUALIZAR_NAVIGATION';
+export const ACTUALIZAR_UBICACION = "ACTUALIZAR_UBICACION";
 
-export const actualizarNavigation = (navigation) => ({
-	type: ACTUALIZAR_NAVIGATION,
-	navigation,
+export const actualizarUbicacion = (routeName) => ({
+  type: ACTUALIZAR_UBICACION,
+  routeName,
 });
 
 const initialState = {};
 
 export default (state = initialState, action) => {
-	switch (action.type) {
-		case ACTUALIZAR_NAVIGATION:
-			return {
-				...state,
-				navigation: action.navigation,
-			};
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case ACTUALIZAR_UBICACION:
+      return {
+        ...state,
+        routeName: action.routeName,
+      };
+    default:
+      return state;
+  }
 };
