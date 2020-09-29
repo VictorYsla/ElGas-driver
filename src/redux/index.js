27dd07a1bb9cc4e98c3c9737310b350ea4cdf30c
Brayan Garcia <bgarcia.95@outlook.com>
2020-09-28 18:12:43 -0600

    Add navegacion reducer to store

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,11 +1,14 @@
 import { combineReducers } from "redux";
 
 import login, { actualizarLogin } from "./reducers/login";
+import navegacion, { actualizarUbicacion } from "./reducers/navigation";
 
 export default combineReducers({
   login,
+  navegacion,
 });
 
 export const actions = {
   actualizarLogin,
+  actualizarUbicacion,
 };
