09be4be406de45b9fcb243c96494231a8f47e92f
Brayan Garcia <bgarcia.95@outlook.com>
2020-09-28 18:15:20 -0600

    Update querys file

--- src/apis/querys.js
+++ src/apis/querys.js
@@ -1,5 +1,17 @@
 import firebase from "firebase";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBk5ZiEvknFcEYFohScW2l_UER61mXwOIs",
+  authDomain: "camp-ebd9b.firebaseapp.com",
+  databaseURL: "https://elgas-68c82.firebaseio.com",
+  projectId: "elgas-68c82",
+  storageBucket: "elgas-68c82.appspot.com",
+  messagingSenderId: "564053304656",
+  appId: "1:564053304656:android:6aeaf58d5780d7c9cb3855",
+};
+
+firebase.initializeApp(firebaseConfig);
+
 export const getCollection = async (collectionName = "1") => {
   return await firebase
     .firestore()
