638c04207f07e8e75a0c2294198f4cdc6257b276
Brayan Garcia <bgarcia.95@outlook.com>
2020-10-05 19:04:20 -0600

    Fix circular size

--- src/components/Modals/LoadingModal.js
+++ src/components/Modals/LoadingModal.js
@@ -4,21 +4,41 @@ import {
   Text,
   StyleSheet,
   TouchableNativeFeedback,
-  FlatList, ScrollView, TouchableOpacity, ActivityIndicator
+  FlatList,
+  ScrollView,
+  TouchableOpacity,
+  ActivityIndicator,
 } from "react-native";
 import { RFPercentage } from "react-native-responsive-fontsize";
-import { widthPercentageToDP as wp, heightPercentageToDP as hp } from "react-native-responsive-screen";
-import Modal from 'react-native-modal';
-import ElGasLogo from '../Icons/ElGasLogo'
-import {colores} from '../../constantes/Temas'
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+import Modal from "react-native-modal";
+import ElGasLogo from "../Icons/ElGasLogo";
+import { colores } from "../../constantes/Temas";
 
-const LoadingModal = ({modal={show:false, message:'Mensaje'}, setModal=()=>{}}) => {
-    const {show, message} = modal 
-    return(
-        <Modal isVisible={show} style={{width:wp(80), height:hp(20), backgroundColor:'transparent', alignSelf:'center', marginVertical:hp(20)}}  backdropOpacity={0.5} >
-            <ActivityIndicator size={hp(15)} />
-        </Modal>
-    )
-}
+const LoadingModal = ({
+  modal = { show: false, message: "Mensaje" },
+  setModal = () => {},
+}) => {
+  const { show, message } = modal;
+  return (
+    <Modal
+      isVisible={show}
+      style={{
+        width: wp(80),
+        height: hp(20),
+        flex: 1,
+        backgroundColor: "transparent",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+      backdropOpacity={0.5}
+    >
+      <ActivityIndicator size='large' color={colores.amarillo} />
+    </Modal>
+  );
+};
 
-export default LoadingModal
+export default LoadingModal;
